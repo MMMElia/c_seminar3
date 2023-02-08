2cdfcb6490cc626c2a590043749ee81eca6e65a2
Elya Megrabyan <m-elia@yandex.ru>
2023-02-08 19:42:51 +0300

    Домашняя задача 23

---
+++ DZ_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write($"Таблица кубов числа {n}: ");
+for ( int i=1; i<=n; i++) {
+    Console.Write($"{Math.Pow(i,3)}, ");
+}
